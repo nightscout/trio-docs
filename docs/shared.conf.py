e78591c461fa022ef7426a2ca65996f0899546da
Mike Plante <mikeplante1@gmail.com>
2024-05-18 15:48:21 -0400

    update website logo

--- docs/shared.conf.py
+++ docs/shared.conf.py
@@ -170,13 +170,13 @@ html_theme_path = []
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
 #html_logo = None
-# html_logo = '../../drawing.png' # path relative to languages conf.py
+html_logo = '../trio-logo.png' # path relative to languages conf.py
 
 # The name of an image file (relative to this directory) to use as a favicon of
 # the docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
 #html_favicon = None
-##html_favicon = '../../favicon.ico' # path relative to languages conf.py
+html_favicon = '../favicon.ico' # path relative to languages conf.py
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
